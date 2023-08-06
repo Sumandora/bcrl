0c076c918040df2d67149d548fb7d22fae12930d
Maxim Mikhaylov <max@sn3k.dev>
2023-08-06 22:21:09 +0200

    remove debug output

--- Example/Main.cpp
+++ Example/Main.cpp
@@ -16,7 +16,6 @@ int main()
 
 	auto session = BCRL::Session::String(newString)
 					   .FindXREFs("bcrlExample", true, false) // main and SuperSecretMethod
-					   .ForEach([](BCRL::SafePointer ptr) { printf("Thing: %p\n", ptr.GetPointer()); })
 					   .Add(4)
 					   .Repeater([](BCRL::SafePointer& ptr) { ptr = ptr.NextInstruction(); return !ptr.Equals<unsigned char>('\xe8'); }) // Find next call instruction
 					   .Add(5)
